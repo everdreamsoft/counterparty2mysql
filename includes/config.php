c3b4e5ed5743d560772ffec622d3f2b9b375d89d
shaban shaame <shaban@everdreamsoft.com>
2020-04-03 10:11:55 +0200

    updating some comments

--- includes/config.php
+++ includes/config.php
@@ -7,34 +7,47 @@
 error_reporting(E_ERROR);
 
 /* Mainnet config */
+/*
 if($runtype=='mainnet'){
     define("DB_HOST", "localhost");
-    define("DB_USER", "mysql_username");
-    define("DB_PASS", "mysql_password");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_DATA", "Hazama");
+    define("CP_HOST", "http://localhost:24000/api/");
+    define("CP_USER", "rpc");
+    define("CP_PASS", "rpc");
+}
+*/
+
+
+if($runtype=='mainnet'){
+    define("DB_HOST", "127.0.0.1");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
     define("DB_DATA", "Counterparty");
-    define("CP_HOST", "http://127.0.0.1:4000/api/");
-    define("CP_USER", "counterparty_username");
-    define("CP_PASS", "counterparty_password");
+    define("CP_HOST", "http://public.coindaddy.io:4000/api/");
+    define("CP_USER", "rpc");
+    define("CP_PASS", "1234");
 }
 
 /* Testnet config */
 if($runtype=='testnet'){
     define("DB_HOST", "localhost");
-    define("DB_USER", "mysql_username");
-    define("DB_PASS", "mysql_password");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
     define("DB_DATA", "Counterparty_Testnet");
-    define("CP_HOST", "http://127.0.0.1:14000/api/");
-    define("CP_USER", "counterparty_username");
-    define("CP_PASS", "counterparty_password");
+    define("CP_HOST", "http://eve.everdreamsoft.com:4000/api/");
+    define("CP_USER", "rpc");
+    define("CP_PASS", "rpcGUISEPPEXCP");
 }
 
 /* Regtest config */
 if($runtype=='regtest'){
     define("DB_HOST", "localhost");
-    define("DB_USER", "mysql_username");
-    define("DB_PASS", "mysql_password");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
     define("DB_DATA", "Counterparty_Regtest");
-    define("CP_HOST", "http://127.0.0.1:44000/api/");
+    define("CP_HOST", "https://eve.everdreamsoft.com/_api");
     define("CP_USER", "counterparty_username");
     define("CP_PASS", "counterparty_password");
 }
